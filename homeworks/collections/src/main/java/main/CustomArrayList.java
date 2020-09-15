657e342d469ca2e53dc2fc764bfae16598c66236
Hakob Khachatryan <hakob.khachatryan2001@gmail.com>
2020-09-15 23:47:08 +0400

    [create] custom ArrayList

---
+++ homeworks/collections/src/main/java/main/CustomArrayList.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.ArrayList;
+
+public class CustomArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        System.out.println(arrayList.get(3));
+        System.out.println(arrayList);
+        arrayList.remove(3);
+        System.out.println(arrayList);
+    }
+}
